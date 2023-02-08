426f0772d271a73df33bb646b7dab39e585c622b
vikiv5 <vi_kov@ukr.net>
2023-02-09 00:19:28 +0200

    import

---
+++ src/Api.js
@@ -0,0 +1,4 @@
+const axios = require('axios').default;
+
+const API = " https://pixabay.com/api/";
+const KEY = "33488620-f3c6f5bf900815c11263bf1e8" ; 
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+import Notiflix from 'notiflix';
+
+
+
+//axios.get('/users')
+ // .then(res => {
+  //  console.log(res.data);
+ // });
