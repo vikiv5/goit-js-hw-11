2e295896fddd2af6a510407ce27a33b2ba30832e
vikiv5 <vi_kov@ukr.net>
2023-02-10 19:15:42 +0200

    api

--- src/Api.js
+++ src/Api.js
@@ -1,7 +1,42 @@
-const axios = require('axios').default;
+import axios from "axios"
+
+//const API = " https://pixabay.com/api/";
+//const KEY = "33488620-f3c6f5bf900815c11263bf1e8"; 
+
+export default class SearchImages {
+    constructor() {
+        this.queryPage=1
+        this.searchQuery = "";
+    }
 
 
+async  fetchImages () {
+    const API_URL = " https://pixabay.com/api/";
+    const KEY_URL = "33488620-f3c6f5bf900815c11263bf1e8"; 
+    const response = await axios.get(`${API_URL}?key=${KEY_URL}&q=${this.searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.queryPage}`
+);
+this.incrementPage()
+return response.data.hits;
 
-const API = " https://pixabay.com/api/";
-const KEY = "33488620-f3c6f5bf900815c11263bf1e8" ; 
+}
 
+resetPage()
+{
+   this.queryPage = 1;
+}
+
+
+    incrementPage()
+   {
+    this.queryPage += 1 ; 
+   }
+
+get query()
+{
+    return this.searchQuery ;
+}
+set query(newQuery)
+{
+    return this.searchQuery=newQuery
+}
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,106 @@
-import axios from "axios";
+
 import Notiflix from 'notiflix';
 
-import SimpleLightbox from "simplelightbox";
+//import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+//import "simplelightbox/dist/simple-lightbox.min.css";
 
-//axios.get('/users')
- // .then(res => {
-  //  console.log(res.data);
- // });
+import SearchImages from './Api';
+import LoadMoreBtn from './loadMoreBtn' ; 
+
+ const form = document.querySelector (".search-form");
+ const galleryImg = document.querySelector ( ".gallery");
+
+ //const submitBtn = document.querySelector (".submit-button");
+
+
+ const loadMoreBtn = new LoadMoreBtn({
+    selector: "#loadMoreBtn",
+    isHidden : true,
+
+ });
+
+ const searchImages = new SearchImages ();
+
+ form.addEventListener ( "submit" , onSubmit);
+ loadMoreBtn.button.addEventListener("click", onLoadBtnClick)
+
+
+ function onSubmit (e){
+    e.preventDefault();
+    const form = e.currentTarget ;
+    searchImages.searchQuery=form.elements.searchQuery.value.trim(); 
+    clearImgList();
+    searchImages.resetPage();
+    loadMoreBtn.show ();
+    onLoadBtnClick()
+ }
+
+ async function onLoadBtnClick() {
+    loadMoreBtn.disable();
+    try {
+        const newSearch = await searchImages.fetchImages()
+
+        if (newSearch.data.hits.length=== 0 ) {
+            Notiflix.Notify.failure ("Sorry, there are no images matching your search query. Please try again.")
+            loadMoreBtn.hide();
+        }
+        else if  (newSearch.data.hits.length < 40 ) {
+            createMarkup(newSearch.data);
+            Notiflix.Notify.failure ("We're sorry, but you've reached the end of search results.")
+            loadMoreBtn.hide();
+        }
+else {
+    createMarkup (newSearch.data)
+    loadMoreBtn.enable();}
+}
+
+    catch (err) {
+        onError(err)
+    }
+    finally{
+        form.reset()
+    }
+ }
+
+ function onError (err) {
+    clearImgList()
+Notiflix.Notify.failure ("Sorry, there are no images matching your search query. Please try again.")
+ }
+
+function createMarkup({hits}){
+    const markup=hits.map (({webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads}) => 
+ ` <div class="photo-card">
+
+ <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>${likes}</b>
+    </p>
+    <p class="info-item">
+      <b>${views}</b>
+    </p>
+    <p class="info-item">
+      <b>${comments}</b>
+    </p>
+    <p class="info-item">
+      <b>${downloads}</b>
+    </p>
+  </div>
+</div> `) 
+.join("");
+galleryImg.insertAdjacentHTML("beforeend", markup)
+gallery.refresh()
+} 
+
+
+
+function clearImgList(){
+    galleryImg.innerHTML = "";
+}
